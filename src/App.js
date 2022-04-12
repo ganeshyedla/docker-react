daa2781907e87ab8bb81eec95fe5edf473c3de35
ganeshyedla <54095804+ganeshyedla@users.noreply.github.com>
2022-04-12 16:58:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there, from Github Actions!</p>
+        <p>Hi there, from Github Actions!!</p>
       </header>
     </div>
   );
